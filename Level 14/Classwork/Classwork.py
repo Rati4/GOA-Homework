3c6ef1b05399105a2a01404fa70672b458f0b1b0
Rati4 <Chrdileli.rati@gmail.com>
2024-08-13 19:05:20 +0400

    fifteenth commit

---
+++ Level 14/Classwork/Classwork.py
@@ -0,0 +1,53 @@
+number1 = "345"
+number2 = 562
+number3 = 3.0
+number4 = "Rati"
+number5 = "Chrdileli"
+
+print (type(number1))
+print (type(number2))
+print (type(number3))
+print (type(number4))
+print (type(number5))
+
+
+
+num1 = 135
+num2 = 5.0
+num3 = "421"
+num4 = "2.0"
+num5 = 7.0
+
+
+print(str(num1))
+print(int(num2))
+print(float(num3))
+print(float(num4))
+print(str(num5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
